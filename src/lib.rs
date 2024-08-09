4c8b2243980a1d7fbf9bcc5e580445c4dd9fcd02
liujingx <liujingx21@mails.tsinghua.edu.cn>
2024-08-09 18:10:52 +0800

    feat: add methods to constant and default-or

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,18 @@ pub trait Generator<T> {
 }
 
 /// Constant generator.
-pub struct Constant<T>(pub T);
+pub struct Constant<T>(T);
+
+impl<T> Constant<T> {
+    /// Create a new constant generator.
+    pub fn new(value: T) -> Self {
+        Self(value)
+    }
+    /// Set the value of the generator.
+    pub fn set(&mut self, value: T) {
+        self.0 = value;
+    }
+}
 
 impl<T> Generator<T> for Constant<T>
 where
@@ -27,7 +38,18 @@ where
 /// Default-or generator.
 ///
 /// Generates the default value of type `T` if the wrapped generator fails.
-pub struct DefaultOr<T, G>(pub G, pub T);
+pub struct DefaultOr<T, G>(T, G);
+
+impl<T, G> DefaultOr<T, G> {
+    /// Create a new default-or generator.
+    pub fn new(default: T, generator: G) -> Self {
+        Self(default, generator)
+    }
+    /// Set the default value of the generator.
+    pub fn set_default(&mut self, default: T) {
+        self.0 = default;
+    }
+}
 
 impl<T, G> Generator<T> for DefaultOr<T, G>
 where
@@ -35,7 +57,7 @@ where
     T: Clone,
 {
     fn try_generate(&mut self) -> Option<T> {
-        self.0.try_generate().or(Some(self.1.clone()))
+        self.1.try_generate().or(Some(self.0.clone()))
     }
 }
 
